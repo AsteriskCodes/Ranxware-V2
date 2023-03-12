bc04214908c9036b0d247ee439243074035cda83
Untyper <127458523+AsteriskCodes@users.noreply.github.com>
2023-03-12 17:34:38 -0500

    Create source

---
+++ source.lua
@@ -0,0 +1,5 @@
+local gameid = game.PlaceId
+local path = "https://raw.githubusercontent.com/AsteriskCodes/Ranxware-V2/main/Games/"
+if gameid == 8365905206 then -- untitled tag game
+	loadstring(game:HttpGet(path.."UntitledTagGame.lua"))()
+end
